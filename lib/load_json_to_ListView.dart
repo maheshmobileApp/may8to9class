9eb3def8887b3a1a6d9d75742a42218a890a0bf4
mahesh <yernagulamahesh@gmail.com>
2023-06-02 22:11:24 +0530

    listview part 2

--- lib/load_json_to_ListView.dart
+++ lib/load_json_to_ListView.dart
@@ -12,11 +12,74 @@ class LoadJsonToListView extends StatefulWidget {
 }
 
 class _LoadJsonToListViewState extends State<LoadJsonToListView> {
+  List<Data> students = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Load Json into ListView")),
-      body: Container(color: Colors.green),
+      body: ListView.separated(
+        itemCount: students.length,
+        itemBuilder: (context, index) {
+          final studentInfo = students[index];
+          return Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  index == 0
+                      ? Image.asset(
+                          "assets/images/kolhi.jpg",
+                          width: 200,
+                          height: 200,
+                        )
+                      : SizedBox(),
+                  Row(
+                    children: [
+                      Text(
+                        "Student Name:",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      Text(studentInfo.studentame ?? ""),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Student Admission Number:",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18)),
+                      Text(studentInfo.admissionNo ?? ""),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Student Class Name:",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18)),
+                      Text(studentInfo.classname ?? ""),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Student Section:",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18)),
+                      Text(studentInfo.section ?? ""),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(
+            height: 5,
+            color: Colors.transparent,
+          );
+        },
+      ),
     );
   }
 
@@ -32,5 +95,9 @@ class _LoadJsonToListViewState extends State<LoadJsonToListView> {
     final resultResponse = await json.decode(resultData);
     final studentData = StudentsData.fromJson(resultResponse);
     print(resultResponse);
+    print(studentData.data);
+    setState(() {
+      students = studentData.data!;
+    });
   }
 }
